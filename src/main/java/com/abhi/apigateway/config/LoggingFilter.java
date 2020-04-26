da868557a4eaca35d8bacb960a4331252bfb03d6
Abhishek Raj <abhishekkumarraj03@gmail.com>
2020-04-26 19:24:34 +0530

    api gateway setup project

---
+++ src/main/java/com/abhi/apigateway/config/LoggingFilter.java
@@ -0,0 +1,34 @@
+package com.abhi.apigateway.config;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class LoggingFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 10000;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        request.getRequestURI();
+
+        return null;
+    }
+}
